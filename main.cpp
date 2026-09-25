ba3ad97dd21ee7a3c23d67afb26bd6bc9faa9576
Utkrista Basnyat <YOUR_GITHUB_EMAIL>
2026-09-25 06:30:23 -0700

    Add multiple Color objects and formatted output

--- main.cpp
+++ main.cpp
@@ -50,13 +50,32 @@ public:
 int main()
 {
     Color color1;
+    Color color2;
+    Color color3;
 
     color1.setRed(255);
     color1.setGreen(100);
     color1.setBlue(50);
 
-    cout << "Color 1" << endl;
+    color2.setRed(0);
+    color2.setGreen(150);
+    color2.setBlue(255);
+
+    color3.setRed(80);
+    color3.setGreen(200);
+    color3.setBlue(120);
+
+    cout << "Color Values" << endl;
+    cout << "------------" << endl;
+
+    cout << "Color 1: ";
     color1.print();
+
+    cout << "Color 2: ";
+    color2.print();
+
+    cout << "Color 3: ";
+    color3.print();
 
     return 0;
 }
